cc5fea4d5ecb4858275bcc018dd342374751270b
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-05 19:19:34 +0100

    added userinfo to header

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -3,47 +3,63 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
-  selector: 'ngx-header',
-  styleUrls: ['./header.component.scss'],
-  templateUrl: './header.component.html',
+    selector: 'ngx-header',
+    providers: [AngularFireAuth],
+    styleUrls: ['./header.component.scss'],
+    templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
 
 
-  @Input() position = 'normal';
+    @Input() position = 'normal';
 
-  user: any;
+    user: any;
+    photoURL: string ;
+    displayName: string;
 
-  userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
+    userMenu = [{ title: 'Profile' }, { title: 'Log out' }];
 
-  constructor(private sidebarService: NbSidebarService,
-              private menuService: NbMenuService,
-              private userService: UserService,
-              private analyticsService: AnalyticsService) {
-  }
+    constructor(private sidebarService: NbSidebarService,
+                private menuService: NbMenuService,
+                private userService: UserService,
+                public afAuth: AngularFireAuth,
+                private analyticsService: AnalyticsService) {
+    }
 
-  ngOnInit() {
-    this.userService.getUsers()
-      .subscribe((users: any) => this.user = users.jack);
-  }
+    ngOnInit() {
+     this.afAuth.auth.onAuthStateChanged( user => {
+            if (user) {
+                console.log(user);
+                this.user = user;
+                this.photoURL = user.photoURL;
+                this.displayName = user.displayName;
 
-  toggleSidebar(): boolean {
-    this.sidebarService.toggle(true, 'menu-sidebar');
-    return false;
-  }
+                // ...
+            } else {
+             //TODO user is signed out
+            }
+        });
 
-  toggleSettings(): boolean {
-    this.sidebarService.toggle(false, 'settings-sidebar');
-    return false;
-  }
+    }
 
-  goToHome() {
-    this.menuService.navigateHome();
-  }
+    toggleSidebar(): boolean {
+        this.sidebarService.toggle(true, 'menu-sidebar');
+        return false;
+    }
 
-  startSearch() {
-    this.analyticsService.trackEvent('startSearch');
-  }
+    toggleSettings(): boolean {
+        this.sidebarService.toggle(false, 'settings-sidebar');
+        return false;
+    }
+
+    goToHome() {
+        this.menuService.navigateHome();
+    }
+
+    startSearch() {
+        this.analyticsService.trackEvent('startSearch');
+    }
 }
