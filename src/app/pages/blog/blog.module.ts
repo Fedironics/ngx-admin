157e3fbfc96e8ae92522bd8021f2118adcceca88
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-05 03:26:49 +0100

    fixed bug of length something

--- src/app/pages/blog/blog.module.ts
+++ src/app/pages/blog/blog.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {BlogComponent} from './blog.component';
-import {ThemeModule} from '../../@theme/theme.module';
+import { BlogComponent } from './blog.component';
+import { ThemeModule } from '../../@theme/theme.module';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { ViewPostComponent } from './view-post/view-post.component';
 import { ViewCommentsComponent } from './view-comments/view-comments.component';
 import { AddCommentComponent } from './view-comments/add-comment/add-comment.component';
-import {BlogRoutingModule} from './blog-routing.module';
+import { BlogRoutingModule } from './blog-routing.module';
 import { GridComponent } from './grid/grid.component';
 
 @NgModule({
@@ -15,7 +15,6 @@ import { GridComponent } from './grid/grid.component';
         ThemeModule,
         BlogRoutingModule,
     ],
-    providers: [ ],
     declarations: [ BlogComponent, CreatePostComponent, ViewPostComponent,
         ViewCommentsComponent, AddCommentComponent, GridComponent ],
 })
