5733a39624d2d63887caaa1d7c56c1f75a8271f2
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-05 18:05:31 +0100

    demo of firebase login

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,20 @@
 export class User {
-    public name: string;
+    public displayName: string;
     public email: string;
-    public imageUrl: string;
+    public uid: string;
+    public photoURL: string;
     public isiMeter: boolean;
     public level: number;
+    public phoneNumber: string;
+    constructor(displayName: string, email: string, photoURL: string, phoneNumber: string, uid: string ){
+        this.displayName = displayName;
+        this.email = email;
+        this.photoURL = photoURL;
+        this.phoneNumber = phoneNumber;
+        this.uid = uid;
+
+        this.isiMeter = false;
+        this.level = 1;
+
+    }
 }
