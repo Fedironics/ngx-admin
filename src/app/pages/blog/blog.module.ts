e6f26ca07a18138d0ff571281f4b5cbd8e298432
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-08 11:51:17 +0100

    added form module and shifted the add comment component

--- src/app/pages/blog/blog.module.ts
+++ src/app/pages/blog/blog.module.ts
@@ -5,16 +5,18 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { ViewPostComponent } from './view-post/view-post.component';
 import { ViewCommentsComponent } from './view-post/view-comments/view-comments.component';
-import { AddCommentComponent } from './view-post/view-comments/add-comment/add-comment.component';
+import { AddCommentComponent } from './view-post/add-comment/add-comment.component';
 import { BlogRoutingModule } from './blog-routing.module';
 import { GridComponent } from './grid/grid.component';
 import { GridItemComponent } from './grid/grid-item/grid-item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
         ThemeModule,
         BlogRoutingModule,
+        FormsModule,
     ],
     declarations: [ BlogComponent, CreatePostComponent, ViewPostComponent,
         ViewCommentsComponent, AddCommentComponent, GridComponent, GridItemComponent ],
