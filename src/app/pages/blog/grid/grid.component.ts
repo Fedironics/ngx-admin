5aaf749a6fc8f34ca338ce477810dcd1bcdcd18c
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-08 11:58:06 +0100

    used firbase datasnapshots to get the key of objects

--- src/app/pages/blog/grid/grid.component.ts
+++ src/app/pages/blog/grid/grid.component.ts
@@ -1,31 +1,24 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NbThemeService } from '@nebular/theme';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAction, AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
-import { Post } from '../../../models/post';
+import * as firebase from 'firebase/database';
+import DataSnapshot = firebase.database.DataSnapshot;
 
 @Component({
     selector: 'ngx-grid',
     templateUrl: './grid.component.html',
     styleUrls: ['./grid.component.scss'],
 })
-export class GridComponent implements OnInit, OnDestroy {
-  public posts: Observable<Post[]>;
-    currentTheme: string;
+export class GridComponent implements OnInit {
+    //TODO : later add subtle animation for at hover effect
+    public paginator: { left: boolean, right: boolean } = { left: true, right: false };
+    public posts: Observable<AngularFireAction<DataSnapshot>[]>;
     themeSubscription: any;
-    constructor(private themeService: NbThemeService, db: AngularFireDatabase) {
-        this.themeSubscription = this.themeService.getJsTheme().subscribe(theme => {
-            this.currentTheme = theme.name;
-        });
-        this.posts = db.list<Post>('blog').valueChanges();
+    constructor(public db: AngularFireDatabase) { }
+
+    ngOnInit() {
+        this.posts = this.db.list('blog').snapshotChanges();
         console.log(this.posts);
     }
 
-
-    ngOnInit() {
-
-    }
-    ngOnDestroy() {
-        this.themeSubscription.unsubscribe();
-    }
 }
