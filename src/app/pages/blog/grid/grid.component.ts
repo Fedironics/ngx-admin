276a70d7b3ccff289b0f13a768b1359c3eccd578
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-15 18:08:53 +0100

    create empty post on button click

--- src/app/pages/blog/grid/grid.component.ts
+++ src/app/pages/blog/grid/grid.component.ts
@@ -3,6 +3,11 @@ import { AngularFireAction, AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/database';
 import DataSnapshot = firebase.database.DataSnapshot;
+import { Router } from '@angular/router';
+import { AngularFireList } from 'angularfire2/database/interfaces';
+import { Post } from '../../../models/post';
+import { User } from '../../../models/user';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
     selector: 'ngx-grid',
@@ -13,12 +18,31 @@ export class GridComponent implements OnInit {
     //TODO : later add subtle animation for at hover effect
     public paginator: { left: boolean, right: boolean } = { left: true, right: false };
     public posts: Observable<AngularFireAction<DataSnapshot>[]>;
+    public postsRef: AngularFireList<any>;
+    public user: User;
     themeSubscription: any;
-    constructor(public db: AngularFireDatabase) { }
+    constructor(private db: AngularFireDatabase, private router: Router, private afAuth: AngularFireAuth) { }
 
     ngOnInit() {
-        this.posts = this.db.list('blog').snapshotChanges();
+        this.postsRef = this.db.list('blog');
+        this.posts = this.postsRef.snapshotChanges();
         console.log(this.posts);
+    }
+
+    createPost() {
+        this.afAuth.auth.onAuthStateChanged(user => {
+            if (user) {
+                console.log(user);
+                this.user = new User(user.displayName,user.email, user.photoURL, user.phoneNumber, user.uid);
+                var key = this.postsRef.push(new Post(this.user)).key;
+                this.router.navigate(['pages/blog/create-post/' + key]);
+                // ...
+            } else {
+                //TODO user is signed out
+            }
+        });
+
+        //TODO : here create the post
     }
 
 }
