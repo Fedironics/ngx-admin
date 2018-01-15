ceb23c10b0bb05d5c7972c84c4d6bea219293d5e
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-15 18:06:08 +0100

    added FOTA module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
 import { BlogModule } from './blog/blog.module';
 import { ProfileComponent } from './profile/profile.component';
+import { FotaModule } from './fota/fota.module';
 
 const PAGES_COMPONENTS = [
   PagesComponent,
@@ -17,6 +18,7 @@ const PAGES_COMPONENTS = [
     ThemeModule,
     DashboardModule,
       BlogModule,
+      FotaModule
   ],
   declarations: [
     ...PAGES_COMPONENTS,
