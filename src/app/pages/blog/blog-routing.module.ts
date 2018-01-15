aad4098d0036fb75ec86abdf8813b3f74e65f3f7
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-15 18:06:48 +0100

    added create post

--- src/app/pages/blog/blog-routing.module.ts
+++ src/app/pages/blog/blog-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {BlogComponent} from './blog.component';
-import {CreatePostComponent} from './create-post/create-post.component';
-import {ViewPostComponent} from './view-post/view-post.component';
-import {GridComponent} from './grid/grid.component';
+import { BlogComponent } from './blog.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { ViewPostComponent } from './view-post/view-post.component';
+import { GridComponent } from './grid/grid.component';
 
 
 const routes: Routes = [{
     path: '',
     component: BlogComponent,
     children: [{
-        path: 'create-post',
-        component: CreatePostComponent,
-    }, {
         path: 'view/:id',
         component: ViewPostComponent,
+    }, {
+        path: 'create-post/:id',
+        component: CreatePostComponent,
     }, {
         path: 'grid',
         component: GridComponent,
