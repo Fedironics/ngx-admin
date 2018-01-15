3904f709071629a4bee7d27a4b102b74755c4a6c
Madunagu Ekene <ekenemadunagu@gmail.com>
2018-01-15 18:03:43 +0100

    firestor uplaod

--- src/app/pages/blog/create-post/create-post.component.ts
+++ src/app/pages/blog/create-post/create-post.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 import { Post } from '../../../models/post';
+import { Upload } from '../../../models/upload';
+import { ActivatedRoute } from "@angular/router";
+import { AngularFireList } from 'angularfire2/database/interfaces';
+import { UploadService } from '../../../@core/data/upload.service';
 
 @Component({
   selector: 'ngx-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.scss'],
+  providers: [UploadService],
 })
 export class CreatePostComponent implements OnInit {
-public post: Post ;
-  constructor() { }
+  public id: string;
+  public post: Post;
+  public postRef: AngularFireObject<Post>;
+  selectedFiles: FileList;
+  currentUpload: Upload;
+  constructor(private db: AngularFireDatabase, private activatedRoute: ActivatedRoute, private upSvc: UploadService) {
 
+  }
+  setImage() {
+    //TODO : here upload the image which has been set to firebase and add it as an attribute of this post
+  }
   ngOnInit() {
+    this.activatedRoute.params.map(params => params['id']).subscribe((id) => this.id = id);
+    this.postRef = this.db.object('blog/' + this.id);
+    this.postRef.valueChanges().subscribe(res => {
+      //    this.post = new Post(res.user); 
+    });
+
+  }
+  detectFiles(event) {
+    this.selectedFiles = event.target.files;
+  }
+  uploadSingle() {
+    let file = this.selectedFiles.item(0)
+    this.currentUpload = new Upload(file);
+    this.upSvc.pushUpload(this.currentUpload)
+  }
+
+  update() {
+    this.postRef.update(this.post);
   }
 
 }
